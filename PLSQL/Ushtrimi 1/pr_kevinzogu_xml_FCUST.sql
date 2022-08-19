b30c0fffae3b77791eb1eaff0caa08333376b180
FACILIZATION\kevin.zogu <kevinzogu@gmail.com>
2022-08-19 12:21:14 +0200

    COMMIT

--- PLSQL/Ushtrimi 1/pr_kevinzogu_xml_FCUST.sql
+++ PLSQL/Ushtrimi 1/pr_kevinzogu_xml_FCUST.sql
@@ -34,7 +34,7 @@ NLS_CHARSET_ID ('UTF8')
    end loop;
    
 EXCEPTION
-  -- when no data found 
+  -- when no data found prit it
   WHEN no_data_found THEN
     dbms_output.put_line('No data found');
     -- when others print the error message
