2b64db990ed2d76a3c10dcbc844d888c1577848f
FACILIZATION\kevin.zogu <kevinzogu@gmail.com>
2022-09-07 17:09:31 +0200

    STARTING EXERCISE 4

---
+++ FlexCube/Ushtrimi 4/STDKEVI4 RAD/JS/STDKEVI4_SYS.js
@@ -0,0 +1,169 @@
+/***************************************************************************************************************************
+**  This source is part of the FLEXCUBE Software Product. 
+**  Copyright (c) 2008 ,2022, Oracle and/or its affiliates.
+**  All rights reserved.
+**  
+**  No part of this work may be reproduced, stored in a retrieval system, 
+**  adopted or transmitted in any form or by any means, electronic, mechanical, photographic, 
+**  graphic, optic recording or otherwise, translated in any language or computer language, 
+**  without the prior written permission of Oracle and/or its affiliates.
+**  
+**  Oracle Financial Services Software Limited.
+**  Oracle Park, Off Western Express Highway,
+**  Goregaon (East),
+**  Mumbai - 400 063,
+**  India.
+**  
+**  Written by         : 
+**  Date of creation   : 
+**  File Name          : STDKEVI4_SYS.js
+**  Purpose            : 
+**  Called From        : 
+**  
+**  CHANGE LOG
+**  Last Modified By   : 
+**  Last modified on   : 
+**  Full Version       : 
+**  Reason             : 
+****************************************************************************************************************************/
+
+//***** Code for criteria Search *****
+var criteriaSearch  = '';
+//----------------------------------------------------------------------------------------------------------------------
+//***** FCJ XML FOR THE SCREEN *****
+//----------------------------------------------------------------------------------------------------------------------
+var fieldNameArray = {"BLK_MASTER":"AMOUNT~CCY~CUSTOMER_ACCOUNT~CUSTOMER_NAME~CUSTOMER_NO~C_DATE~EMAIL~PHONE_NUMBER~PRIORITY~MAKER~MAKERSTAMP~CHECKER~CHECKERSTAMP~MODNO~TXNSTAT~AUTHSTAT~ONCEAUTH","BLK_DETAIL":"CUSTOMER_ID~CUSTOMER_NO~DETAIL_DESCRIPTION~DETAIL_USER~D_DATE~SUBJECT","BLK_HEADER":""};
+
+var multipleEntryPageSize = {"BLK_DETAIL" :"15" };
+
+var multipleEntrySVBlocks = "";
+
+var tabMEBlks = {"CVS_MAIN__TAB_DETAIL":"BLK_DETAIL"};
+
+var msgxml=""; 
+msgxml += '    <FLD>'; 
+msgxml += '      <FN PARENT="" RELATION_TYPE="1" TYPE="BLK_MASTER">AMOUNT~CCY~CUSTOMER_ACCOUNT~CUSTOMER_NAME~CUSTOMER_NO~C_DATE~EMAIL~PHONE_NUMBER~PRIORITY~MAKER~MAKERSTAMP~CHECKER~CHECKERSTAMP~MODNO~TXNSTAT~AUTHSTAT~ONCEAUTH</FN>'; 
+msgxml += '      <FN PARENT="BLK_MASTER" RELATION_TYPE="N" TYPE="BLK_DETAIL">CUSTOMER_ID~CUSTOMER_NO~DETAIL_DESCRIPTION~DETAIL_USER~D_DATE~SUBJECT</FN>'; 
+msgxml += '      <FN PARENT="" RELATION_TYPE="1" TYPE="BLK_HEADER"></FN>'; 
+msgxml += '    </FLD>'; 
+
+var strScreenName = "CVS_MAIN";
+var qryReqd = "Y";
+var txnBranchFld = "" ;
+var originSystem = "";
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR DATABINDING *****
+//----------------------------------------------------------------------------------------------------------------------
+ var relationArray = {"BLK_MASTER" : "","BLK_DETAIL" : "BLK_MASTER~N","BLK_HEADER" : ""}; 
+
+ var dataSrcLocationArray = new Array("BLK_MASTER","BLK_DETAIL","BLK_HEADER"); 
+ // Array of all Data Sources used in the screen 
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR QUERY MODE *****
+//----------------------------------------------------------------------------------------------------------------------
+var detailRequired = true ;
+var intCurrentQueryResultIndex = 0;
+var intCurrentQueryRecordCount = 0;
+
+var queryFields = new Array();    //Values should be set inside STDKEVI4.js, in "BlockName__FieldName" format
+var pkFields    = new Array();    //Values should be set inside STDKEVI4.js, in "BlockName__FieldName" format
+queryFields[0] = "BLK_MASTER__CUSTOMER_NO";
+pkFields[0] = "BLK_MASTER__CUSTOMER_NO";
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR AMENDABLE/SUBSYSTEM Fields *****
+//----------------------------------------------------------------------------------------------------------------------
+var modifyAmendArr = new Array(); 
+var closeAmendArr = new Array(); 
+var reopenAmendArr = new Array(); 
+var reverseAmendArr = new Array(); 
+var deleteAmendArr = new Array(); 
+var rolloverAmendArr = new Array(); 
+var confirmAmendArr = new Array(); 
+var liquidateAmendArr = new Array(); 
+var queryAmendArr = new Array(); 
+var authorizeAmendArr = new Array(); 
+//----------------------------------------------------------------------------------------------------------------------
+
+var subsysArr    = new Array(); 
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//***** CODE FOR LOVs *****
+//----------------------------------------------------------------------------------------------------------------------
+var lovInfoFlds = {};
+var offlineLovInfoFlds = {};
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR TABS *****
+//----------------------------------------------------------------------------------------------------------------------
+var strHeaderTabId = 'TAB_HEADER';
+var strFooterTabId = 'TAB_FOOTER';
+var strCurrentTabId = 'TAB_MAIN';
+//--------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR MULTIPLE ENTRY BLOCKS *****
+//----------------------------------------------------------------------------------------------------------------------
+var multipleEntryIDs = new Array("BLK_DETAIL");
+var multipleEntryArray = new Array();
+var multipleEntryCells = new Array();
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR MULTIPLE ENTRY VIEW SINGLE ENTRY BLOCKS *****
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR ATTACHED CALLFORMS *****
+ //----------------------------------------------------------------------------------------------------------------------
+
+ var CallFormArray= new Array(); 
+
+ var CallFormRelat=new Array(); 
+
+ var CallRelatType= new Array(); 
+
+
+ var ArrFuncOrigin=new Array();
+ var ArrPrntFunc=new Array();
+ var ArrPrntOrigin=new Array();
+ var ArrRoutingType=new Array();
+
+
+ // Code for Loading Cluster/Custom js File Starts
+ var ArrClusterModified=new Array();
+ var ArrCustomModified=new Array();
+ // Code for Loading Cluster/Custom js File ends
+
+ArrFuncOrigin["STDKEVI4"]="CUSTOM";
+ArrPrntFunc["STDKEVI4"]="";
+ArrPrntOrigin["STDKEVI4"]="";
+ArrRoutingType["STDKEVI4"]="X";
+
+
+ // Code for Loading Cluster/Custom js File Starts
+ArrClusterModified["STDKEVI4"]="N";
+ArrCustomModified["STDKEVI4"]="Y";
+
+ // Code for Loading Cluster/Custom js File ends
+
+
+ /* Code For OBIEE functionalities */ 
+var obScrArgName  = new Array(); 
+var obScrArgSource  = new Array(); 
+//***** CODE FOR SCREEN ARGS *****
+//----------------------------------------------------------------------------------------------------------------------
+var scrArgName = {};
+var scrArgSource = {};
+var scrArgVals = {};
+var scrArgDest = {};
+//***** CODE FOR SUB-SYSTEM DEPENDENT  FIELDS   *****
+//----------------------------------------------------------------------------------------------------------------------
+var dpndntOnFlds = {};
+var dpndntOnSrvs = {};
+//***** CODE FOR TAB DEPENDENT  FIELDS   *****
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR CALLFORM TABS *****
+//----------------------------------------------------------------------------------------------------------------------
+var callformTabArray = new Array(); 
+//***** CODE FOR ACTION STAGE DETAILS *****
+//----------------------------------------------------------------------------------------------------------------------
+var actStageArry = {};
+//***** CODE FOR IMAGE FLDSET *****
+//----------------------------------------------------------------------------------------------------------------------
